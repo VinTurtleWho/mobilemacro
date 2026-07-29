b79d51880fc59a0c63fac10aba38170e68daa439
MobileMacro <macro@example.com>
2026-07-29 12:31:25 +0530

    Fix missing float type in TickRecord constructor

--- src/main/java/com/mobilemacrofarm/TickRecord.java
+++ src/main/java/com/mobilemacrofarm/TickRecord.java
@@ -4,7 +4,7 @@ public class TickRecord {
     public final boolean w, a, s, d, space, sneak, attack, use;
     public final float yaw, pitch;
 
-    public TickRecord(boolean w, boolean a, boolean s, boolean d, boolean space, boolean sneak, boolean attack, boolean use, float yaw, pitch) {
+    public TickRecord(boolean w, boolean a, boolean s, boolean d, boolean space, boolean sneak, boolean attack, boolean use, float yaw, float pitch) {
         this.w = w; this.a = a; this.s = s; this.d = d;
         this.space = space; this.sneak = sneak;
         this.attack = attack; this.use = use;
