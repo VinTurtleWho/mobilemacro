a5f6bdc5ae2f21c11e5e0b00d0c1b40f8333f62e
MobileMacro <macro@example.com>
2026-07-30 13:55:52 +0530

    Removed reflection, using native handleInventoryMouseClick for Loom compilation

--- src/main/java/com/mobilemacrofarm/FarmingMacro.java
+++ src/main/java/com/mobilemacrofarm/FarmingMacro.java
@@ -100,18 +100,9 @@ public class FarmingMacro {
     }
 
     private void safeClick(Minecraft client, AbstractContainerScreen<?> screen, Slot slot) {
-        try {
-            Class<?> clickTypeClass = Class.forName("net.minecraft.world.inventory.ClickType"); Object pickup = null;
-            for (Object ec : clickTypeClass.getEnumConstants()) { if (ec.toString().equals("QUICK_MOVE") || ec.toString().equals("SHIFT_CLICK")) { pickup = ec; break; } }
-            boolean clicked = false;
-            for (Method m : client.gameMode.getClass().getMethods()) {
-                String n = m.getName();
-                if ((n.equals("handleInventoryMouseClick") || n.equals("clickSlot") || n.equals("method_2906")) && m.getParameterCount() == 5) {
-                    m.invoke(client.gameMode, screen.getMenu().containerId, slot.index, 0, pickup, client.player); clicked = true; break;
-                }
-            }
-            if (!clicked && client.player != null) client.player.sendSystemMessage(Component.literal("§c[MobileMacro] Click method not found! (Obfuscation issue)"));
-        } catch (Exception e) { }
+        if (client.gameMode != null && client.player != null) {
+            client.gameMode.handleInventoryMouseClick(screen.getMenu().containerId, slot.index, 0, net.minecraft.world.inventory.ClickType.QUICK_MOVE, client.player);
+        }
     }
 
     @SuppressWarnings("unchecked")
