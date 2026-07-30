371d51df0f59e8f06b738ed0ddeda18442137541
MobileMacro <macro@example.com>
2026-07-30 14:00:06 +0530

    Added bulletproof signature-based reflection for chest clicker

--- src/main/java/com/mobilemacrofarm/FarmingMacro.java
+++ src/main/java/com/mobilemacrofarm/FarmingMacro.java
@@ -100,9 +100,22 @@ public class FarmingMacro {
     }
 
     private void safeClick(Minecraft client, AbstractContainerScreen<?> screen, Slot slot) {
-        if (client.gameMode != null && client.player != null) {
-            client.gameMode.handleInventoryMouseClick(screen.getMenu().containerId, slot.index, 0, net.minecraft.world.inventory.ClickType.QUICK_MOVE, client.player);
-        }
+        try {
+            for (Method m : client.gameMode.getClass().getMethods()) {
+                Class<?>[] p = m.getParameterTypes();
+                if (p.length == 5 && p[0] == int.class && p[1] == int.class && p[2] == int.class && p[3].isEnum() && net.minecraft.world.entity.player.Player.class.isAssignableFrom(p[4])) {
+                    Object action = null;
+                    for (Object ec : p[3].getEnumConstants()) {
+                        String n = ec.toString();
+                        if (n.equals("QUICK_MOVE") || n.equals("SHIFT_CLICK")) { action = ec; break; }
+                    }
+                    if (action != null) {
+                        m.invoke(client.gameMode, screen.getMenu().containerId, slot.index, 0, action, client.player);
+                    }
+                    break;
+                }
+            }
+        } catch (Exception e) { }
     }
 
     @SuppressWarnings("unchecked")
